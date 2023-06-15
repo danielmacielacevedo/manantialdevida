7f790d3630c6dd2de36550d5627b0b709d6a285c
Daniel Maciel Acevedo <anndannma@outlook.com>
2023-06-15 11:36:14 -0600

    Agregué administradores

--- pages/api/Administradores.js
+++ pages/api/Administradores.js
@@ -1,9 +1,9 @@
 // Administradores.js
 
 const Administradores = [
-  // "2v8vhcmISgeZKEDk2YDACmGgDJI2",
+  "2v8vhcmISgeZKEDk2YDACmGgDJI2",
 
-  // "U9UMfAa0LCh6f9klUhj2sK4Bx683",
+  "U9UMfAa0LCh6f9klUhj2sK4Bx683",
   "LJtncOdSYLdWIrNmYQCxE0al54q1",
   "RUQgNwDtNLQU438wdwzTPhaMWxH2",
 ];
